d46afd90e221b41bcac096c365fe1281a0494e99
Francisco Huertas <francisco@fhuertas.com>
2013-08-13 10:58:31 +0200

    Arregkando heroku

--- web.js
+++ web.js
@@ -18,12 +18,12 @@
 	  response.send('Hello World!');
 	});
 
-	var port = process.env.PORT || 5000;
+	var port = process.env.PORT || 5050;
 	app.listen(port, function() {
 	  console.log("Listening on " + port);
 	});
 	
-	return
+	return;
 	var parser = require('properties-parser');
 	
 	
